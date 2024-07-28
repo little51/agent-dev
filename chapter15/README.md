0cf3d32ed11a4010251b6fa252d3f2ab8f739e7a
simon gao <little51@126.com>
2024-07-28 12:15:24 +0800

    完成第15章

---
+++ chapter15/README.md
@@ -0,0 +1,59 @@
+# Qwen-Agent案例
+
+
+## 一、Qwen-VL服务安装配置
+
+```shell
+# 1、获取服务代码
+git clone https://github.com/little51/agent-dev
+cd agent-dev/chapter15
+# 2、创建虚拟环境
+conda create -n qwen-vl python=3.10 -y
+# 3、激活虚拟环境
+conda activate qwen-vl
+# 4、安装依赖库
+pip install -r requirements-openai-api.txt \
+-i https://pypi.mirrors.ustc.edu.cn/simple
+# 5、下载Qwen-VL-Chat模型
+# （1）获取模型下载脚本
+wget https://e.aliendao.cn/model_download.py
+# （2）下载模型到dataroot/models/Qwen/Qwen-VL-Chat
+python model_download.py --e --repo_id Qwen/Qwen-VL-Chat \
+--token YPY8KHDQ2NAHQ2SG
+# 6、运行Qwen-VL服务\
+CUDA_VISIBLE_DEVICES=0 python qwenvl-openai-api.py \
+-c dataroot/models/Qwen/Qwen-VL-Chat \
+--server-name 0.0.0.0
+```
+
+
+## 二、Qwen-Agent环境安装
+
+```shell
+# 1、创建虚拟环境
+conda create -n qwen-agent python=3.10 -y
+# 2、激活虚拟环境
+conda activate qwen-agent
+# 3、安装依赖库
+# （1）安装qwen-agent
+pip install qwen-agent==0.0.6 \
+-i https://pypi.mirrors.ustc.edu.cn/simple
+# （2）安装modelscope-studio
+pip install modelscope-studio==0.4.0 \
+-i https://pypi.mirrors.ustc.edu.cn/simple
+# （3）安装gradio
+pip install gradio==4.37.2 \
+-i https://pypi.mirrors.ustc.edu.cn/simple
+```
+
+## 三、运行程序
+
+```shell
+conda activate qwen-agent
+python qwen-agent-sample.py
+# 测试
+# 访问：http://172.16.62.167:7860/
+# 输入
+# 文本：识别图片中的方程组
+# 图片：test1.png 或 test2.png
+```
---
+++ chapter15/qwen-agent-sample.py
@@ -0,0 +1,57 @@
+import copy
+from typing import Dict, Iterator, List, Optional, Union
+from qwen_agent import Agent
+from qwen_agent.tools import BaseTool
+from qwen_agent.agents import Assistant
+from qwen_agent.llm import BaseChatModel
+from qwen_agent.llm.schema import Message
+from qwen_agent.gui import WebUI
+
+llm_config = {'model': 'qwenvl_oai',
+              'model_server': 'http://127.0.0.1:8000/v1',
+              'api_key': 'EMPTY'}
+
+
+class Visual_solve_equations(Agent):
+    def __init__(self,
+                 function_list: Optional[
+                     List[Union[str,
+                                Dict, BaseTool]]] = None,
+                 llm: Optional[Union[Dict, BaseChatModel]] = None):
+        super().__init__(llm=llm)
+        # 定义图片识别Agent
+        self.image_agent = Assistant(llm=self.llm)
+        # 定义数学计算Agent
+        self.math_agent = Assistant(
+            llm=self.llm,
+            system_message='你扮演一个学生，参考你学过的数学知识进行计算')
+
+    def _run(self, messages: List[Message],
+             lang: str = 'zh', **kwargs) -> Iterator[List[Message]]:
+        # 校验WebUI传入的参数，必须为list，且包含图片
+        assert isinstance(messages[-1]['content'], list)
+        assert any([item.image for item in messages[-1]['content']]
+                   ), '这个智体应用需要输入图片'
+        response = []
+        # 第1个Agent，将图片内容识别成文本
+        new_messages = copy.deepcopy(messages)
+        new_messages[-1].content[0]['text'] = str(
+            [{"text": new_messages[-1].content[0]['text']}, {
+                "image": new_messages[-1].content[1]['image'].replace("file://", "")}])
+        for rsp in self.image_agent.run(new_messages, lang=lang, **kwargs):
+            yield rsp
+        # 第2个Agen，求解文本中的数学问题
+        response = rsp
+        new_messages.extend(rsp)
+        new_messages.append(Message('user', '根据以上文本内容求解数学题'))
+        for rsp in self.math_agent.run(new_messages, lang=lang, **kwargs):
+            yield response + rsp
+
+
+def app_gui():
+    bot = Visual_solve_equations(llm=llm_config)
+    WebUI(bot).run(server_name="0.0.0.0")
+
+
+if __name__ == '__main__':
+    app_gui()
